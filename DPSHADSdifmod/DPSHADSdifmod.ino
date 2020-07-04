75320937efac29910a8442e881cddd336a0032ec
IsauraRs <isaurars97@gmail.com>
2020-07-03 19:52:18 -0500

    Muestra los datos en processing

--- DPSHADSdifmod/DPSHADSdifmod.ino
+++ DPSHADSdifmod/DPSHADSdifmod.ino
@@ -1,6 +1,6 @@
 //Este codigo abarca el manejo del sensor de efecto hall,
 //el potenciometro digital X9C103S
-//Y el ADS1115 en modo diferencial.
+//Y el ADS1115 en modo diferencial
 //Version que envia datos a processing, usar este
 
 //BotRelevador
@@ -75,9 +75,10 @@ void setup(void)
  
 void loop(void) 
 {
-  //Hall();
-  //ADS11l();
+  
   DP();
+
+  //Activa el relay por medio de telegram
   if(Serial.available()>0)
   {
     estado=Serial.read();
@@ -94,52 +95,20 @@ void loop(void)
   
 }
 
-/*void Hall()
-{
-  //Efecto Hall
-  delay(999);//Update RPM every second
-  
-  detachInterrupt(0); //Interrupts are disabled
-
-  rpm = 60*contador;
-  
-  passedtime = millis();
-  
-  contador = 0;
-  //Serial.print("RPM=");
-  
-  Serial.println(rpm); //Print out result to monitor
-  
-  attachInterrupt(0, isr, RISING);   //Restart the interrupt processing
-}
-
-void ADS11l()
-{
-  // Obtiene datos del pin A0 del ADS1115 y los imprime
-  short adc0 = ads.readADC_SingleEnded(0);
-  //Serial.print("\t\t A0: "); 
-  Serial.println(adc0);
- 
-  delay(1000);
-}*/
 
 void DP()
 {
 
   //Potenciometro digital
-  //Serial.println("\t\t\t Potenciometro Digital");  
-
   for (int i=0; i<1023; i++) {
-    //0,1023
-    //Serial.print("Aumentando, i = ");
+
     Serial.print("ValDP: ");
     Serial.println(i, DEC);
     pot.increase(1);
-    //Serial.print(",");
     //Efecto Hall
-    delay(999);//Update RPM every second
+    delay(999);//Actualiza RPM cada segundo
     
-    detachInterrupt(0); //Interrupts are disabled
+    detachInterrupt(0); //Se deshabilitan las interrupciones
   
     rpm = 60*contador;
     
@@ -148,18 +117,13 @@ void DP()
     contador = 0;
     Serial.print("RPM= ");
     
-    Serial.println(rpm); //Print out result to monitor
+    Serial.println(rpm); //Imprime las RPM
     
-    attachInterrupt(0, isr, RISING);   //Restart the interrupt processing
-    
-    //Serial.print(",");
+    attachInterrupt(0, isr, RISING);   //Reinicia el interruptor de inicio
     
     delay(1000);
     
-    // Obtiene datos del pin A0 del ADS1115 y los imprime
-    //short adc0 = ads.readADC_SingleEnded(0);
-    //Serial.print("ADS: "); 
-    //Serial.println(adc0);
+    // Obtiene datos del pin A0 y A1 del ADS1115 y los imprime
 
     short diferencia_0_1 = ads.readADC_Differential_0_1();
     float volts = (diferencia_0_1 * factorEscala)/1000.0;
@@ -168,26 +132,21 @@ void DP()
     Serial.print("Voltaje: ");
     Serial.println(volts,4);
    
-    //delay(1000);
-
-    //Serial.print(",");
     //Delay del DP
     delay(1000);
-    //Serial.println();
+
   }
 
   for (int i=0; i<1023; i++) {
     Serial.print("ValDP:");
-//Serial.print("Disminuyendo, i = ");
 
     Serial.println(i, DEC);
     pot.decrease(1);
     
-    //Serial.print(",");
     //Efecto Hall
-    delay(999);//Update RPM every second
+    delay(999);//Actualiza las RPM cada segundo
     
-    detachInterrupt(0); //Interrupts are disabled
+    detachInterrupt(0); //Se deshabilitan las interrupciones
   
     rpm = 60*contador;
     
@@ -196,31 +155,23 @@ void DP()
     contador = 0;
     Serial.print("RPM= ");
     
-    Serial.println(rpm); //Print out result to monitor
+    Serial.println(rpm); //Imprime RPM
     
-    attachInterrupt(0, isr, RISING);   //Restart the interrupt processing
-    //Serial.print(",");
+    attachInterrupt(0, isr, RISING);   //Reinicia el interruptor 
     
     delay(1000);
     
-    // Obtiene datos del pin A0 del ADS1115 y los imprime
-    //short adc0 = ads.readADC_SingleEnded(0);
-    //Serial.print("ADS: "); 
-    //Serial.println(adc0);
-
+    // Obtiene datos del pin A0 y A1 del ADS1115 y los imprime
     short diferencia_0_1 = ads.readADC_Differential_0_1();
     float volts = (diferencia_0_1 * factorEscala)/1000.0;
     Serial.print("Diferencia 0-1");    
     Serial.println(diferencia_0_1);
     Serial.print("Voltaje: ");
     Serial.println(volts,4);
- 
-    //delay(1000);
-
-    //Serial.print(",");
     //Delay del DP
     
     delay(1000);
-    //Serial.println();
+
   }
+
 }
