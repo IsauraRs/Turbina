06f03a6505fe5c7e626de1536b719d76a6a04f61
IsauraRs <isaurars97@gmail.com>
2020-07-13 22:39:37 -0500

    Lee los datos enviados por el módulo BT en python

--- creacion_base/datos_Serial.py
+++ creacion_base/datos_Serial.py
@@ -2,11 +2,13 @@
 import serial
 
 #Check if the serial port is the same you're using in arduino
-arduino = serial.Serial('/dev/ttyACM0', 9600)
+arduino = serial.Serial('/dev/rfcomm0', 9600)
 arduino_lectures = [] #The list is declared
 
 while True:
     values = arduino.readline()
     vd = values.decode() #Decodes the values from byte to string
-    arduino_lectures.append(vd) #Adds the values to the list
-    print(arduino_lectures)
+    arduino_lectures.append(vd) #Adds the values to the list  
+    for i in range(len(arduino_lectures)):
+        n = arduino_lectures[i].replace('\r\n', '') #Removes the \r\n characters from the string and replace them with a space
+    print(n)
