25f5f1adb9a40d51fa8ba4dc3b28563456149789
IsauraRs <isaurars97@gmail.com>
2020-08-01 23:26:38 -0500

    Lee los datos enviados por el módulo BT en python

--- creacion_base/datos_Serial.py
+++ creacion_base/datos_Serial.py
@@ -4,18 +4,19 @@ import csv
 import Models1 as conex
 
 #Check if the serial port is the same you're using in arduino
-#arduino = serial.Serial('/dev/rfcomm0', 9600)
-arduino = serial.Serial('/dev/ttyUSB0', 9600)
+arduino = serial.Serial('/dev/rfcomm0', 9600)
+#arduino = serial.Serial('/dev/ttyUSB0', 9600)
 arduino_lectures = [] #The list is declared
 
-while True:
-    values = arduino.readline()
-    vd = values.decode() #Decodes the values from byte to string
-    arduino_lectures.append(vd) #Adds the values to the list 
-    nl=[]
-    for i in range(len(arduino_lectures)):
-        n = arduino_lectures[i].replace('\r\n', '') #Removes the \r\n characters from the string and replace them with a space
-        nl.append(n) #Adds to the list nl the values without the "\r\n" characters
-    if len(nl) > 4:
-        conex.carga(nl[0],nl[1],nl[2],nl[3]) #Loads the values sent from arduino to the table
-    print(nl)
+def testc():
+    while True:
+        values = arduino.readline()
+        vd = values.decode() #Decodes the values from byte to string
+        arduino_lectures.append(vd) #Adds the values to the list 
+        nl=[]
+        for i in range(len(arduino_lectures)):
+            n = arduino_lectures[i].replace('\r\n', '') #Removes the \r\n characters from the string and replace them with a space
+            nl.append(n) #Adds to the list nl the values without the "\r\n" characters
+        if len(nl) > 4:
+            conex.carga(nl[0],nl[1],nl[2],nl[3]) #Loads the values sent from arduino to the table
+        print(nl)
