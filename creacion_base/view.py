ea2230c0ac027285ee7c3d234a2a2f20f2a15848
IsauraRs <isaurars97@gmail.com>
2020-08-02 23:59:18 -0500

    Vista

---
+++ creacion_base/view.py
@@ -0,0 +1,19 @@
+import psycopg2
+from tkReg import *
+
+host = 'ec2-34-225-82-212.compute-1.amazonaws.com'
+
+database= 'd60lbn7ubp9jlb'
+user= 'kgsvsidlipqnoy'
+password='3e3c54e483c6797261dad22ef4735c24fa2b8df1fc993252f572bb1618019073'
+
+def vista(dpVal):
+    conexion = psycopg2.connect(host=host, database=database, user=user, password=password)
+    cursor = conexion.cursor()
+    a = "SELECT valor_pot_digt FROM lectura WHERE valor_pot_digt = " + " ' " + (dpVal) + " ' " + ";"
+    v = cursor.execute(a)
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+    print(v)
+    
