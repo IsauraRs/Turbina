645db1b23c6711f426451ecf75ddb037df2d8b1f
galigaribaldi <galigaribaldi@live.com>
2020-08-03 00:26:47 -0500

    Correcciones al Script

--- creacion_base/view.py
+++ creacion_base/view.py
@@ -10,10 +10,10 @@ password='3e3c54e483c6797261dad22ef4735c24fa2b8df1fc993252f572bb1618019073'
 def vista(dpVal):
     conexion = psycopg2.connect(host=host, database=database, user=user, password=password)
     cursor = conexion.cursor()
-    a = "SELECT valor_pot_digt FROM lectura WHERE valor_pot_digt = " + " ' " + (dpVal) + " ' " + ";"
-    v = cursor.execute(a)
+    cursor.execute("SELECT valor_pot_digt FROM lectura WHERE valor_pot_digt = %s", (dpVal,))
+    datos = cursor.fetchall()
     conexion.commit()
     cursor.close()
     conexion.close()
-    print(v)
-    
+    print(datos)
+#vista(str(570))
