a99f07c85297a502512c953783bda2b03ad149aa
IsauraRs <isaurars97@gmail.com>
2020-11-30 14:29:03 -0600

    Se añaden agregan columnas de potencia y eficiencia a las tablas de carga

--- Web/Models1Cargar.py
+++ Web/Models1Cargar.py
@@ -6,10 +6,10 @@ database= 'd60lbn7ubp9jlb'
 user= 'kgsvsidlipqnoy'
 password='3e3c54e483c6797261dad22ef4735c24fa2b8df1fc993252f572bb1618019073'
 
-def carga(param2, param3, param4, param5, param6, param7, param8):
+def carga(param2, param3, param4, param5, param6, param7, param8, param9, param10, param11):
     conexion = psycopg2.connect(host=host, database=database, user=user, password=password)
     cursor = conexion.cursor()
-    cursor.execute("INSERT INTO lectura(valor_pot_digt, rev_min, dif_01, voltaje, tiemp , voltajein, dif_23) VALUES (%s, %s, %s, %s, %s , %s, %s);",(param2, param3, param4, param5, param6, param7, param8))
+    cursor.execute("INSERT INTO lectura(valor_pot_digt, rev_min, dif_01, voltaje_in, dif_23, voltaje_out, tiemp, potencia, ef_generador, ef_turbina) VALUES (%s, %s, %s, %s, %s , %s, %s, %s, %s , %s);",(param2, param3, param4, param5, param6, param7, param8, param9, param10, param11))
     conexion.commit()
     cursor.close()
     conexion.close()
