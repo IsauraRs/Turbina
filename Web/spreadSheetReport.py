997077afcc6def89d69c478069a7cba08636fbf3
IsauraRs <isaurars97@gmail.com>
2020-12-09 01:17:24 -0600

    Se añaden graficas al reporte

--- Web/spreadSheetReport.py
+++ Web/spreadSheetReport.py
@@ -12,7 +12,9 @@ row4 = []
 
 filesheet = "ReporteSpreadsheet.xlsx"
 wb = load_workbook(filesheet)
-graph = wb.create_sheet(index = 2, title = "Gráficas")
+graph = wb.create_sheet(index = 2, title = "RvsRPM")
+graph2 = wb.create_sheet(index = 3, title = "RvsEfm")
+graph3 = wb.create_sheet(index = 4, title = "RvsEft")
 #datos = obtener_etiquetas(an2.ciudades,an2.edad_por_grupos,an2.sexo,an2.nse, an2.marcas)
 sheet = wb.active
 
@@ -50,20 +52,82 @@ def setData(data):
 def createGraphs(disl,disl1): #,disl2,disl3):
     #for i in range(1,len(disl)):
 
+    res = 'Resistencia'
+    rps = 'RPM'
+    disl = [res] + disl
+    disl1 = [rps] + disl1
+
+
     for g in range(1): 
         graph.append(disl)
         graph.append(disl1)
     
     chart = ScatterChart()
-    chart.title = 'GraficaP'
-    chart.x_axis.title = 'y'
-    chart.y_axis.title = 'x'
-    xvals = Reference(graph, min_col=1,min_row=1,max_col=len(disl))
-    for s in range(1,len(disl)): #10
-        values = Reference(graph,min_col=1,min_row=2,max_col=len(disl))
-        series = Series(values,xvals,title_from_data=True)
-        chart.series.append(series)
+    chart.title = 'R[Ω] vs RPM'
+    chart.x_axis.title = 'RPM'
+    chart.y_axis.title = 'R[Ω]'
+    xvals = Reference(graph, min_col=2,min_row=1,max_col=len(disl))
+    #for s in range(1,len(disl)): #10
+    values = Reference(graph,min_col=2,min_row=2,max_col=len(disl))
+    series = Series(values,xvals,title_from_data=True)
+    chart.series.append(series)
     #chart.add_data(values)
     #s = chart.series[1]
     graph.add_chart(chart,"M1")
     wb.save(filesheet)
+
+def createGraph2(disl,disl2): #,disl2,disl3):
+    
+    res = 'Resistencia'
+    efg = 'Ef.Generador'
+    disl = [res] + disl
+    disl2 = [efg] + disl2
+
+    for g in range(1): 
+        graph2.append(disl)
+        graph2.append(disl2)
+        
+    
+    chart = ScatterChart()
+    chart.title = 'R[Ω] vs Eficiencia Generador'
+    chart.x_axis.title = 'Efm'
+    chart.y_axis.title = 'R[Ω]'
+    xvals = Reference(graph2, min_col=2,min_row=1,max_col=len(disl))
+    #for s in range(1,len(disl)): #10
+    values = Reference(graph2,min_col=2,min_row=2,max_col=len(disl))
+    series = Series(values,xvals,title_from_data=True)
+    chart.series.append(series)
+    #chart.add_data(values)
+    #s = chart.series[1]
+    graph2.add_chart(chart,"M1")
+    wb.save(filesheet)
+
+def createGraph3(disl,disl3): #,disl2,disl3):
+    
+    res = 'Resistencia'
+    eft = 'Ef.Turbina'
+    disl = [res] + disl
+    disl3 = [eft] + disl3
+
+
+    #for i in range(1,len(disl)):
+
+    for g in range(1): 
+        graph3.append(disl)
+        graph3.append(disl3)
+        
+    
+    chart = ScatterChart()
+    chart.title = 'R[Ω] vs Eficiencia turbina'
+    chart.x_axis.title = 'Eft'
+    chart.y_axis.title = 'R'
+    xvals = Reference(graph3, min_col=2,min_row=1,max_col=len(disl))
+    #for s in range(1,len(disl)): #10
+    values = Reference(graph3,min_col=2,min_row=2,max_col=len(disl))
+    series = Series(values,xvals,title_from_data=True)
+    chart.series.append(series)
+    #chart.add_data(values)
+    #s = chart.series[1]
+    graph3.add_chart(chart,"M1")
+    wb.save(filesheet)
+
